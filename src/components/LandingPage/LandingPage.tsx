cd2dcb75cbc255af83d637b298358477e60ee619
Anna Wiśniowska <anna.cherry17@gmail.com>
2022-11-09 13:30:29 +0100

    SERQ - 8 error message in progress

--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -11,23 +11,38 @@ const navigate = useNavigate();
 
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const [error, setError] = useState<null | string>(null)
 
-    const handleLogin = async (e : React.FormEvent<HTMLFormElement>) => {
+    const handleLogin = (e : React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        try {
-            await signInWithEmailAndPassword(auth, email, password);
-            console.log('zalogowano')
-            navigate('/home')
-        } catch (error){
-            console.log(error)
-            try {
-                createUserWithEmailAndPassword(auth, email, password);
-                console.log('utworzono użytkownika')
-                navigate('/home')
-            } catch (error) {
-                console.log('sory, nie wejdziesz')
+        // try {
+        //     await signInWithEmailAndPassword(auth, email, password);
+        //     navigate('/home')
+        // } catch (error){
+        //     // nie ma konta, robimy nowe
+        //     try {
+        //         createUserWithEmailAndPassword(auth, email, password);
+        //         navigate('/home')
+        //         console.log('banan')
+        //     } catch (error) {
+        //         console.log(error)
+        //         setError(true)
+        //     }
+        // }
+        signInWithEmailAndPassword(auth, email, password).then((r) => {
+            navigate('home')
+            console.log(r)
+        }).catch( ({ message }) => {
+            if (message === 'Firebase: Error (auth/wrong-password).')  {
+                setError("Niepoprawny email lub hasło")
             }
-        }
+            createUserWithEmailAndPassword(auth, email, password).then((r) => {
+                navigate('home')
+                console.log(r)
+            }).catch(({ message }) => {
+                console.log(message === 'Firebase: Error (auth/email-already-in-use).')
+            })
+        })
     }
     
  return (
@@ -43,6 +58,7 @@ const navigate = useNavigate();
                     <input type="email" placeholder="Wpisz email" name='email' onChange={(e) => setEmail(e.target.value)} required/>
                     <input type="password" placeholder="Wpisz hasło" name='password' onChange={(e) => setPassword(e.target.value)} required/>
                     <button type='submit'>Zaloguj</button>
+                    { error ? <p>{error}</p> : null}
                 </form>
             </div>
         </div>
