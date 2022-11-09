22467356586b5a50bfb7f12fcc56aa451339c2ea
Anna Wiśniowska <anna.cherry17@gmail.com>
2022-11-09 11:29:45 +0100

    SERQ - 8 done - login function

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.css';
 import {Routes, Route} from 'react-router-dom';
 import { LandingPage } from './components/LandingPage/LandingPage';
 
+
 function App() {
   return (
     <Routes>
--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -1,14 +1,34 @@
 import styles from './LandingPage.module.css';
+import {auth} from '../../firebase'
+import {createUserWithEmailAndPassword} from 'firebase/auth'
+import {signInWithEmailAndPassword} from 'firebase/auth'
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom'
 
-export const LandingPage = () => {
 
-    // const [login, setLogin] = useState('')
-    // const [password, setPassword] = useState('')
+export const LandingPage : React.FC = () => {
+const navigate = useNavigate();
 
-    // const onLogin = () => {
-    //     const user = { login, password };
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
 
-    // }
+    const handleLogin = async (e : React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        try {
+            await signInWithEmailAndPassword(auth, email, password);
+            console.log('zalogowano')
+            navigate('/home')
+        } catch (error){
+            console.log(error)
+            try {
+                createUserWithEmailAndPassword(auth, email, password);
+                console.log('utworzono użytkownika')
+                navigate('/home')
+            } catch (error) {
+                console.log('sory, nie wejdziesz')
+            }
+        }
+    }
     
  return (
     <div className={styles.wrapper}>
@@ -19,9 +39,9 @@ export const LandingPage = () => {
         <div className={styles.formWrapper}>
             <h2>Dołącz</h2>
             <div>
-                <form>
-                    <input type="email" placeholder="Wpisz email" name='email' required/>
-                    <input type="password" placeholder="Wpisz hasło" name='password' required/>
+                <form onSubmit={handleLogin}>
+                    <input type="email" placeholder="Wpisz email" name='email' onChange={(e) => setEmail(e.target.value)} required/>
+                    <input type="password" placeholder="Wpisz hasło" name='password' onChange={(e) => setPassword(e.target.value)} required/>
                     <button type='submit'>Zaloguj</button>
                 </form>
             </div>
