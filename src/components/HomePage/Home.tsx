a17a202e529aff7390acb88cd43c0657f08040f6
Magdalena Zalewska <magdazalewska23@gmail.com>
2022-11-10 09:29:18 +0100

    SERQ-10 adding Leaflet map

--- src/components/HomePage/Home.tsx
+++ src/components/HomePage/Home.tsx
@@ -1,3 +1,25 @@
+import { MapContainer, TileLayer, useMap, Marker, Popup } from "react-leaflet";
+import styles from "./Home.module.css";
+
 export const Home: React.FC = () => {
-  return <div>Home</div>;
+	return (
+		<div>
+			<MapContainer
+				center={[54.352024, 18.646639]}
+				zoom={13}
+				scrollWheelZoom={true}
+				className={styles.wrapper}
+			>
+				<TileLayer
+					attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+					url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+				/>
+				<Marker position={[51.505, -0.09]}>
+					<Popup>
+						A pretty CSS3 popup. <br /> Easily customizable.
+					</Popup>
+				</Marker>
+			</MapContainer>
+		</div>
+	);
 };
