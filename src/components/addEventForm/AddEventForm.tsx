4e0f1a6faa5c83e145b5aab3dca2422fceb1ad76
Anna Wiśniowska <anna.cherry17@gmail.com>
2022-11-17 10:06:25 +0100

    global state changes

--- src/components/addEventForm/AddEventForm.tsx
+++ src/components/addEventForm/AddEventForm.tsx
@@ -5,103 +5,101 @@ import { AuthContext } from "../../providers/global";
 import styles from "./AddEventForm.module.css";
 
 const AddEventForm = () => {
-	const [name, setName] = useState("");
-	const [description, setDescription] = useState("");
-	const [date, setDate] = useState("");
-	const [time, setTime] = useState("");
-	const [category, setCategory] = useState("");
-	const [participants, setParticipants] = useState([]);
-	const { position, user, fetchEvents, allEvents } = useContext(AuthContext);
+  // const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [date, setDate] = useState("");
+  const [time, setTime] = useState("");
+  const [category, setCategory] = useState("");
+  const [participants, setParticipants] = useState([]);
+  const { position, user, fetchEvents, allEvents, name, setName } =
+    useContext(AuthContext);
 
-	//   interface addForm {
-	//     name: string;
-	//     setName: (name: string) => void;
-	//     description: string;
-	//     setDescription: (name: string) => void;
-	//   }
+  //   interface addForm {
+  //     name: string;
+  //     setName: (name: string) => void;
+  //     description: string;
+  //     setDescription: (name: string) => void;
+  //   }
 
-	const addEvent = async (e: FormEvent): Promise<void> => {
-		e.preventDefault();
-		await addDoc(collection(db, "events"), {
-			name: name,
-			description: description,
-			position: position,
-			email: user,
-			date: date,
-			time: time,
-			category: category,
-			participants: participants,
-		});
-		setName("");
-		setDescription("");
-		setTime("");
-		setDate("");
-		setCategory("");
-		setParticipants([]);
-		fetchEvents();
-	};
+  const addEvent = async (e: FormEvent): Promise<void> => {
+    e.preventDefault();
+    await addDoc(collection(db, "events"), {
+      //   name: name,
+      description: description,
+      position: position,
+      email: user,
+      date: date,
+      time: time,
+      category: category,
+      participants: participants,
+    });
+    // setName("");
+    setDescription("");
+    setTime("");
+    setDate("");
+    setCategory("");
+    setParticipants([]);
+    fetchEvents();
+  };
 
-	return (
-		<div className={styles.FormWrapper}>
-			{/* <input placeholder="latitude"></input>
+  return (
+    <div className={styles.FormWrapper}>
+      {/* <input placeholder="latitude"></input>
         <input placeholder="longtitude"></input> */}
-			<form
-				onSubmit={addEvent}
-				className={styles.form}
-			>
-				<input
-					placeholder="imię"
-					onChange={(e) => {
-						setName(e.target.value);
-					}}
-					value={name}
-					required
-				/>
+      <form onSubmit={addEvent} className={styles.form}>
+        {/* <input
+          placeholder="imię"
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+          value={name}
+          required
+        /> */}
 
-				<select
-					name="category"
-					id="category-select"
-					onChange={(e) => {
-						setCategory(e.target.value);
-					}}
-					value={category}
-					required
-				>
-					<option value="">Wybierz kategorię</option>
-					<option value="sport">Sport</option>
-					<option value="nauka">Nauka</option>
-					<option value="kultura">Kultura</option>
-				</select>
-				<input
-					placeholder="data"
-					onChange={(e) => {
-						setDate(e.target.value);
-					}}
-					value={date}
-					type="date"
-					required
-				/>
-				<input
-					placeholder="godzina"
-					onChange={(e) => {
-						setTime(e.target.value);
-					}}
-					value={time}
-					type="time"
-					required
-				/>
-				<textarea
-					placeholder="opis"
-					onChange={(e) => {
-						setDescription(e.target.value);
-					}}
-					value={description}
-					required
-				/>
-				<button>Dodaj wydarzenie</button>
-			</form>
-		</div>
-	);
+        <select
+          name="category"
+          id="category-select"
+          onChange={(e) => {
+            setCategory(e.target.value);
+          }}
+          value={category}
+          required
+        >
+          <option value="">Wybierz kategorię</option>
+          <option value="sport">Sport</option>
+          <option value="nauka">Nauka</option>
+          <option value="kultura">Kultura</option>
+        </select>
+        <input
+          placeholder="data"
+          onChange={(e) => {
+            setDate(e.target.value);
+          }}
+          value={date}
+          type="date"
+          required
+        />
+        <input
+          placeholder="godzina"
+          onChange={(e) => {
+            setTime(e.target.value);
+          }}
+          value={time}
+          type="time"
+          required
+        />
+        <textarea
+          placeholder="opis"
+          onChange={(e) => {
+            setDescription(e.target.value);
+          }}
+          value={description}
+          required
+        />
+        <button>Dodaj wydarzenie</button>
+      </form>
+    </div>
+  );
 };
 
 export default AddEventForm;
