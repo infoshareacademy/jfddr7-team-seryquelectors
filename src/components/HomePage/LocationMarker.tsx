2a351d0989bbdb6f7c017f2a2b85aa2a0b58bd0a
Anna Wiśniowska <anna.cherry17@gmail.com>
2022-11-21 12:13:02 +0100

    app finished :)

--- src/components/HomePage/LocationMarker.tsx
+++ src/components/HomePage/LocationMarker.tsx
@@ -3,12 +3,13 @@ import { useContext, useState } from "react";
 import { AuthContext } from "../../providers/global";
 import { DocumentData } from "firebase/firestore";
 import { greenIcon, goldIcon, violetIcon } from "../../images/Icon";
+import EventDetails from "../EventDetails/EventDetails";
 
 const LocationMarker = () => {
   const { position, setPosition, allEvents, setShowForm, user, filter } =
     useContext(AuthContext);
   const [toggleMarker, setToggleMarker] = useState(false);
-
+  const [visible, setVisible] = useState(false);
   const map = useMapEvents({
     click(e) {
       console.log(e);
@@ -36,7 +37,12 @@ const LocationMarker = () => {
           eventIcon = goldIcon;
         }
         return (
-          <Marker key={i} position={e.position} icon={eventIcon}>
+          <Marker
+            key={i}
+            position={e.position}
+            icon={eventIcon}
+            eventHandlers={{ click: () => setVisible(true) }}
+          >
             <Tooltip>
               <>
                 <b>
